8624e2087016a28e94fb72412ba5ffeaa186120f
Andra <andra.nistor@seznam.cz>
2020-05-12 20:37:20 +0200

    Ukolnicek s fuknci updateTasks

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('funguju!');
+const tasks = [];
+const todo__tasksElm = document.querySelector('.todo__tasks');
+const newTaskElm = document.querySelector('#new-task');
+const btnElm = document.querySelector('.btn-add');
+
+
+const updateTasks = () => {
+  if (newTaskElm.value.trim() !== '') {
+    tasks.push(newTaskElm.value);
+  }
+  todo__tasksElm.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    todo__tasksElm.innerHTML +=
+      `<div class="task">
+    ${tasks[i]}
+    </div>`
+  }
+};
+
+btnElm.addEventListener('click', updateTasks);
