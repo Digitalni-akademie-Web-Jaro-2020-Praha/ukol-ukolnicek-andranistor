8c3b0d365fbd5b9be0da1f6075f0e376c2a2dc53
Andra <andra.nistor@seznam.cz>
2020-05-13 11:22:27 +0200

    Rozdeleni do dvou funkci

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const todo__tasksElm = document.querySelector('.todo__tasks');
 const newTaskElm = document.querySelector('#new-task');
 const btnElm = document.querySelector('.btn-add');
 
-
+// Fce provede aktualizaci dle promenne task
 const updateTasks = () => {
-  if (newTaskElm.value.trim() !== '') {
-    tasks.push(newTaskElm.value);
-  }
   todo__tasksElm.innerHTML = '';
   for (let i = 0; i < tasks.length; i++) {
     todo__tasksElm.innerHTML +=
@@ -19,4 +16,13 @@ const updateTasks = () => {
   }
 };
 
-btnElm.addEventListener('click', updateTasks);
+// Fce na update ukolu
+const newTask = () => {
+  if (newTaskElm.value.trim() !== '') {
+    tasks.push(newTaskElm.value);
+  }
+  updateTasks();
+  newTaskElm.value = '';
+};
+
+btnElm.addEventListener('click', newTask);
